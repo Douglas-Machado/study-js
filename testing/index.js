b280d5169246b049e48f85d68b05130415a35228
Douglas C Machado <douglasclaudino10.dc@gmail.com>
2022-06-23 17:50:30 -0300

    study

--- testing/index.js
+++ testing/index.js
@@ -1,14 +1,28 @@
-// let num = 777
+// segundo menor em um array
 
-// function descendingOrder(num){
-//   let str = num.toString()
-//   const stringArray = str.split('').sort()
-//   const newString = stringArray.reverse().toString()
-//   const result = newString.replace(/,/g, '')
-//   console.log(result)
-// }
+let arr = [1,5,2,7,2,7,4,-34,10,6,3,9,-20,11,-1,14,3,0]
 
-// descendingOrder(num)
-const a = new Date()
-const date = new Date(a).toUTCString()
-console.log(date)
+arr.sort((a, b) => a - b)
+
+console.log(arr[1])
+console.log(arr)
+/* -------------------------------------------------------------- */
+
+// mescle dois arrays ordenados
+
+let a = sort([5,7,3,1])
+let b = sort([6,4,2,8])
+
+let newArr = []
+
+for(let i = 0; i < b.length; i++){
+  newArr.push(a[i], b[i])
+}
+
+function sort(arr) {
+  return arr.sort((a,b) => a - b)
+}
+
+// console.log(newArr)
+
+/* -------------------------------------------------------------- */
