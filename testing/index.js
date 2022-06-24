025ce47c752283f73d463c46621e94663c2f58a3
Douglas C Machado <douglasclaudino10.dc@gmail.com>
2022-06-24 20:15:08 -0300

    study-js

--- testing/index.js
+++ testing/index.js
@@ -4,8 +4,8 @@ let arr = [1,5,2,7,2,7,4,-34,10,6,3,9,-20,11,-1,14,3,0]
 
 arr.sort((a, b) => a - b)
 
-console.log(arr[1])
-console.log(arr)
+// console.log(arr[1])
+// console.log(arr)
 /* -------------------------------------------------------------- */
 
 // mescle dois arrays ordenados
@@ -26,3 +26,8 @@ function sort(arr) {
 // console.log(newArr)
 
 /* -------------------------------------------------------------- */
+
+let string = "hi, I'm douglas and I'm from Brazil"
+
+const aaa = string.replace(/\s+/g,'-')
+console.log(aaa)
